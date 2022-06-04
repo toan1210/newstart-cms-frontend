eaa2864616d4d1e3cdcc1a9e3b35051fa37f2721
minhtoan1210 <nguyenvuminhtoan1999@gmail.com>
2022-06-04 12:15:23 +0700

    up load

--- src/pages/story/component/Item.jsx
+++ src/pages/story/component/Item.jsx
@@ -4,13 +4,22 @@ import useAuth from '../../../core/useAuth';
 
 // import { Container } from './styles';
 
-function Item({_id,title,author,status,category,date,images,summary}) {
+function Item({_id,title,author,status,category,date,images,sumary}) {
   let {ip} = useAuth();
   return(
       <>
       <div className="list-news__itme">
-        <div dangerouslySetInnerHTML={{__html:summary}}></div>
-      </div>
+      <Link to={`/layoutstory/story/${_id}`}>
+                <img src={ip+"images/"+images} alt="" srcSet />
+          </Link>
+          <div className="news-itme__content">
+          <Link to={`/layoutstory/story/${_id}`}>
+              <span>
+               {title}
+              </span>
+            </Link>
+          </div>
+        </div>
       </>
   )
 }
